a6ea1fdc8a31d88f9c63913984bb67980e1f589c
Dmitry <45429986+Endlesssuply@users.noreply.github.com>
2019-02-27 18:05:51 +0300

    Class Board

---
+++ tetris.py
@@ -0,0 +1,44 @@
+import pygame
+
+pygame.init()
+size = 500, 500
+screen = pygame.display.set_mode(size)
+
+
+class Board:
+    # создание поля
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # значения по умолчанию
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    def render(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                pygame.draw.rect(screen, pygame.Color(255, 255, 255), (
+                    x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size, self.cell_size), 1)
+
+    # настройка внешнего вида
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+
+# поле 6 на 8
+board = Board(6, 8)
+board.set_view(50, 50, 50)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    board.render()
+    pygame.display.flip()
+pygame.quit()
